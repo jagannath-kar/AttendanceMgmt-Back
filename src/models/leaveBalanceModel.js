2ef7894a5f9c035a1f925198b958037a0af91c07
neha-cirgir <neha.cirgir@cognizant.com>
2025-11-06 15:12:06 +0530

    changes in leave balance

--- src/models/leaveBalanceModel.js
+++ src/models/leaveBalanceModel.js
@@ -1,17 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const leaveTypeSchema = new mongoose.Schema({
-//   total: { type: Number, required: true, default: 0 }
-// });
-
-// const leaveSchema = new mongoose.Schema({
-//   leaveId: { type: mongoose.Schema.Types.ObjectId, required: true },
-//   sick: { type: leaveTypeSchema, required: true, default: () => ({ total: 0 }) },
-//   casual: { type: leaveTypeSchema, required: true, default: () => ({ total: 0 }) }
-// });
-
-// // module.exports = mongoose.model('Leave', leaveSchema);
-// module.exports = mongoose.model('Leave', leaveSchema, 'leaveBalance');
 const mongoose = require('mongoose');
 
 const leaveTypeSchema = new mongoose.Schema({
